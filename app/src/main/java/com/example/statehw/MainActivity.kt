79c39c053531483405449c7a7238e8dfeb6035c4
Admin <reteamboss@gmail.com>
2024-11-15 16:39:16 +0300

    2

--- app/src/main/java/com/example/statehw/MainActivity.kt
+++ app/src/main/java/com/example/statehw/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.statehw
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -19,18 +20,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,17 +49,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val productList = mutableListOf<Products>(Products.APPLE, Products.CUCUMBER, Products.PINEAPPLE,
-            Products.CORN, Products.TOMATO, Products.BREAD, Products.BUTTER, Products.WATER,
-            Products.MEAT, Products.MILK, Products.EGGS)
 
 
         setContent {
 
-            var count = 3
-            var titleProducts by rememberSaveable() { mutableStateOf("Список продуктов") }
-            var changeLanguageText by rememberSaveable() { mutableStateOf("Switch language") }
-            var rusLanguage by rememberSaveable() { mutableStateOf(true) }
+            var city = remember { mutableStateOf("") }
+            val cityList = remember { mutableStateListOf<String>() }
 
             Column(modifier = Modifier
                 .padding(horizontal = 8.dp)
@@ -69,7 +69,7 @@ class MainActivity : ComponentActivity() {
                             .border(1.dp, color = Color.Black)
                     ) {
 
-                        Text(text = titleProducts,
+                        Text(text = "Динамический список",
                             fontSize = 22.sp,
                             fontWeight = FontWeight.Bold, color = Color.White,
                             modifier =  Modifier.background(color = Color.DarkGray).padding(8.dp))
@@ -87,32 +87,51 @@ class MainActivity : ComponentActivity() {
                         .border(1.dp, color = Color.Black)
 
                 ) {
-                    items(productList) {
-                        product ->
-                        ProductRow(product,rusLanguage)
+                    itemsIndexed(cityList) {
+                        index, city ->
+
+                        Row (verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .wrapContentHeight()
+                                .fillMaxWidth()
+                                .background(Color.White, shape = CircleShape)
+                                .clip(shape = CircleShape)) {
+
+                            Text(text = city,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Normal,
+                                color = Color.Black,
+                                modifier = Modifier.clickable( onClick = {
+                                    cityList.removeAt(index)
+                                }))
+
+                        }
+
                         Spacer(modifier = Modifier.padding(2.dp))
                     }
+
                 }
 
                 Row (horizontalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxWidth())
 
                 {
-                    Text(text = changeLanguageText, fontSize = 18.sp, fontWeight = FontWeight.Bold,
+                    TextField( value = city.value, textStyle = TextStyle(fontSize = 24.sp),
+                        onValueChange = { newText -> city.value = newText},
+                        modifier = Modifier.padding(top = 16.dp))
+                }
+
+                Row (horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth())
+
+                {
+                    Text(text = "Добавить", fontSize = 18.sp, fontWeight = FontWeight.Bold,
                         modifier = Modifier
                             .padding(top = 16.dp)
                             .clickable(onClick = {
-
-                                count += 1
-                                if (count % 2 != 0) {
-                                    titleProducts = "List of products"
-                                    changeLanguageText = "Изменить язык"
-                                    rusLanguage = false
-                                } else {
-                                    titleProducts = "Список продуктов"
-                                    changeLanguageText = "Switch language"
-                                    rusLanguage = true
-                                }} ))
+                                cityList.add(city.value)
+                            } ))
                 }
             }
 
@@ -120,49 +139,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun ProductRow(products: Products, rusLanguage: Boolean) {
-
-    Row (verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .wrapContentHeight()
-            .fillMaxWidth()
-            .background(Color.White, shape = CircleShape)
-            .clip(shape = CircleShape)) {
-
-        if (rusLanguage == true) {
-            Text(text = products.rusName,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color.Black
-            )
-        } else {
-            Text(text = products.engName,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color.Black
-            )
-        }
-    }
-}
 
 
 
-enum class Products(val rusName: String, val engName: String) {
-    APPLE("Яблоко","Apple"),
-    PINEAPPLE("Ананас","Pineapple"),
-    TOMATO("Помидор","Tomato"),
-    CUCUMBER("Огурец","Cucumber"),
-    CORN("Кукуруза","Corn"),
-    WATER("Вода","Water"),
-    BREAD("Хлеб","Bread"),
-    BUTTER("Масло","Butter"),
-    MEAT("Мясо","Meat"),
-    MILK("Молоко","Milk"),
-    EGGS("Яйца","Eggs");
 
-
-
-}
 
